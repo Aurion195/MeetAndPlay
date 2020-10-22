362815446c5bb4c8c95ebb200edc69ad175b16fe
slowblitz <jeremygarcia937@gmail.com>
2020-10-22 16:00:14 +0200

    [@Slowblitz][corsfix]

--- back/src/main.rs
+++ back/src/main.rs
@@ -2,9 +2,16 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 use serde::{Deserialize, Serialize};
+extern crate rocket_cors;
 
 use rocket_contrib::json::Json;
 use rocket_contrib::databases::rusqlite::Connection;
+use rocket::http::Method; // 1.
+
+use rocket_cors::{
+    AllowedHeaders, AllowedOrigins, Error, // 2.
+    Cors, CorsOptions // 3.
+};
 
 #[database("db")]
 struct DbConn(Connection);
@@ -22,6 +29,29 @@ struct JsonApiResponse {
     data: Vec<Event>,
 }
 
+
+fn make_cors() -> Cors {
+    let allowed_origins = AllowedOrigins::some_exact(&[ // 4.
+        "http://localhost:8080",
+        "http://127.0.0.1:8080",
+        "http://localhost:8000",
+        "http://0.0.0.0:8000",
+    ]);
+
+    CorsOptions { // 5.
+        allowed_origins,
+        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(), // 1.
+        allowed_headers: AllowedHeaders::some(&[
+            "Authorization",
+            "Accept",
+            "Access-Control-Allow-Origin", // 6.
+        ]),
+        allow_credentials: true,
+        ..Default::default()
+    }
+    .to_cors()
+    .expect("error while building CORS")
+}
 #[get("/geteventbyid/<id>")]
 fn geteventbyid(id: i32,conn:DbConn) -> Json<JsonApiResponse>  {
   let mut response = JsonApiResponse { data: vec![], };
@@ -35,13 +65,15 @@ fn geteventbyid(id: i32,conn:DbConn) -> Json<JsonApiResponse>  {
           }
         }).unwrap().map(|event| event.unwrap());//response
        for event in events {
+         
          response.data.push(event);}
       Json(response)
 }
 
 #[get("/getallevent")]
 fn getallevent(conn:DbConn) -> Json<JsonApiResponse> {
-  let mut response = JsonApiResponse { data: vec![], };
+  
+  let mut response = JsonApiResponse {data: vec![], };
   let mut stmt = conn.prepare("SELECT id, link, title, img FROM events").unwrap();
   let events = stmt.query_map(&[], |row|  {
           Event{
@@ -57,8 +89,10 @@ fn getallevent(conn:DbConn) -> Json<JsonApiResponse> {
 }
 
 fn main() {
+  
     rocket::ignite()
     .attach(DbConn::fairing())
+    .attach(make_cors()) // 7.
     .mount("/", routes![getallevent,geteventbyid])
     .launch();
 }
