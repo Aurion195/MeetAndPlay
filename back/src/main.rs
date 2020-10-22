e2fb265a608547ea8e76960b71c65b606e5c23b4
slowblitz <jeremygarcia937@gmail.com>
2020-10-22 16:24:28 +0200

    [@Slowblitz][fixcors]

--- back/src/main.rs
+++ back/src/main.rs
@@ -2,16 +2,12 @@
 #[macro_use] extern crate rocket;
 #[macro_use] extern crate rocket_contrib;
 use serde::{Deserialize, Serialize};
-extern crate rocket_cors;
-
+use rocket::{Request, Response};
+use rocket::fairing::{Fairing, Info, Kind};
+use rocket::http::{Header, ContentType, Method};
+use std::io::Cursor;
 use rocket_contrib::json::Json;
 use rocket_contrib::databases::rusqlite::Connection;
-use rocket::http::Method; // 1.
-
-use rocket_cors::{
-    AllowedHeaders, AllowedOrigins, Error, // 2.
-    Cors, CorsOptions // 3.
-};
 
 #[database("db")]
 struct DbConn(Connection);
@@ -26,35 +22,37 @@ struct Event {
 
 #[derive(Serialize)]
 struct JsonApiResponse {
+   
     data: Vec<Event>,
 }
+pub struct CORS();
 
-
-fn make_cors() -> Cors {
-    let allowed_origins = AllowedOrigins::some_exact(&[ // 4.
-        "http://localhost:8080",
-        "http://127.0.0.1:8080",
-        "http://localhost:8000",
-        "http://0.0.0.0:8000",
-    ]);
-
-    CorsOptions { // 5.
-        allowed_origins,
-        allowed_methods: vec![Method::Get].into_iter().map(From::from).collect(), // 1.
-        allowed_headers: AllowedHeaders::some(&[
-            "Authorization",
-            "Accept",
-            "Access-Control-Allow-Origin", // 6.
-        ]),
-        allow_credentials: true,
-        ..Default::default()
+impl Fairing for CORS {
+    fn info(&self) -> Info {
+        Info {
+            name: "Add CORS headers to requests",
+            kind: Kind::Response
+        }
     }
-    .to_cors()
-    .expect("error while building CORS")
+
+    fn on_response(&self, request: &Request, response: &mut Response) {
+        if request.method() == Method::Options || response.content_type() == Some(ContentType::JSON) {
+            response.set_header(Header::new("Access-Control-Allow-Origin", "*"));
+            response.set_header(Header::new("Access-Control-Allow-Methods", "POST, GET, OPTIONS"));
+            response.set_header(Header::new("Access-Control-Allow-Headers", "Content-Type"));
+            response.set_header(Header::new("Access-Control-Allow-Credentials", "true"));
+        }
+
+        if request.method() == Method::Options {
+            response.set_header(ContentType::Plain);
+            response.set_sized_body(Cursor::new(""));
+        }
+    }
 }
+
 #[get("/geteventbyid/<id>")]
 fn geteventbyid(id: i32,conn:DbConn) -> Json<JsonApiResponse>  {
-  let mut response = JsonApiResponse { data: vec![], };
+  let mut response = JsonApiResponse {data: vec![], };
   let mut stmt = conn.prepare("SELECT id, link, title, img FROM events WHERE id=?").unwrap();
   let events = stmt.query_map(&[&id], |row|  {
           Event{
@@ -65,7 +63,6 @@ fn geteventbyid(id: i32,conn:DbConn) -> Json<JsonApiResponse>  {
           }
         }).unwrap().map(|event| event.unwrap());//response
        for event in events {
-         
          response.data.push(event);}
       Json(response)
 }
@@ -92,7 +89,7 @@ fn main() {
   
     rocket::ignite()
     .attach(DbConn::fairing())
-    .attach(make_cors()) // 7.
+    .attach(CORS())
     .mount("/", routes![getallevent,geteventbyid])
     .launch();
 }
