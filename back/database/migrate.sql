f18b5a8e9786d4cad5b92c526a04d5e6753dd726
slowblitz <jeremygarcia937@gmail.com>
2020-10-23 18:50:51 +0200

    [@Slowblitz][adding event and user]

--- back/database/migrate.sql
+++ back/database/migrate.sql
@@ -1,10 +1,16 @@
-"""CREATE TABLE events (
+CREATE TABLE events (
         id INTEGER PRIMARY KEY,
-        link TEXT NOT NULL,
-        title  TEXT NOT NULL,
-        Img TEXT NOT NULL,
-        description TEXT NOT NULL
-);"""
+        Description TEXT NOT NULL,
+        Requierement TEXT NOT NULL,
+        Prix INTEGER,
+        Placerestante INTEGER,
+        Lieux TEXT NOT NULL,
+        Date TEXT NOT NULL,
+        titre  TEXT NOT NULL,
+        Img TEXT NOT NULL
+       
+);
+
 CREATE TABLE Users (
         id INTEGER PRIMARY KEY,
         Pseudo TEXT NOT NULL,
