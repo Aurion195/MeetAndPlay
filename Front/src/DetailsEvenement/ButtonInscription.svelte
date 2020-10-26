727decd3072bbc070d34734f0f5f5faf480beca2
Aurion195 <mathieuthomas84300@hotmail.fr>
2020-10-26 17:15:00 +0100

    [TMU] ajout ecran login

--- Front/src/DetailsEvenement/ButtonInscription.svelte
+++ Front/src/DetailsEvenement/ButtonInscription.svelte
@@ -1,11 +1,19 @@
 <script>
-    import { Router, Link, Route } from "svelte-routing";
+    function alerte() {
+        alert("Votre inscription est en cour merci pour votre confiance") ;
+    }
 </script>
 
 <section class="section buttonEventInscriptionEvent">
     <div class="button">
-        <a class="waves-effect waves-light btn">S'inscrire</a>
+        <a onclick="alerte()" href="/" class="waves-effect waves-light btn">S'inscrire</a>
     </div>
+
+    <script>
+        function alerte() {
+            alert("Votre inscription est en cour merci pour votre confiance") ;
+        }
+    </script>
 </section>
 
 <style>
--- Front/src/User/Login.svelte
+++ Front/src/User/Login.svelte
@@ -1,16 +1,20 @@
-<div class="row">
-    <form class="col s12">
-      <div class="row">
-        <div class="input-field col s6">
-          <i class="material-icons prefix">account_circle</i>
-          <input id="icon_prefix" type="text" class="validate">
-          <label for="icon_prefix">First Name</label>
-        </div>
-        <div class="input-field col s6">
-          <i class="material-icons prefix">phone</i>
-          <input id="icon_telephone" type="tel" class="validate">
-          <label for="icon_telephone">Telephone</label>
-        </div>
+<section class="section Login">
+  <form class="col s12">
+      <div class="input-field col s6">
+        <i class="material-icons prefix">account_circle</i>
+        <input id="icon_prefix" type="text" class="validate">
+        <label for="icon_prefix">Username</label>
       </div>
-    </form>
-  </div>
+      <div class="input-field col s6">
+        <i class="material-icons prefix">lock</i>
+        <input id="icon_telephone" type="password" class="validate">
+        <label for="icon_telephone">Password</label>
+      </div>
+  </form>
+</section>
+
+<style>
+  .section Login {
+    margin: 25% auto;
+  }
+</style>
