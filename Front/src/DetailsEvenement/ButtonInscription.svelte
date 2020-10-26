6537db937cbdae2e602d16e5576a649b8dd13090
Aurion195 <mathieuthomas84300@hotmail.fr>
2020-10-26 13:16:01 +0100

    [TMU] vue details evenement]

---
+++ Front/src/DetailsEvenement/ButtonInscription.svelte
@@ -0,0 +1,19 @@
+<script>
+    import { Router, Link, Route } from "svelte-routing";
+</script>
+
+<section class="section buttonEventInscriptionEvent">
+    <div class="button">
+        <a class="waves-effect waves-light btn">S'inscrire</a>
+    </div>
+</section>
+
+<style>
+    .buttonEvent {
+       margin: 1% ;
+    }
+
+    .button {
+       text-align: center;
+    }
+</style>
